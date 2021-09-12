c7ede48d21a838804eb053b38740604d06098669
Maycon Carvalho <maycon.carvalho@sapiensia.com>
2021-09-12 20:31:37 -0300

    Limpando 2

--- mcarvalho/Controllers/HomeController.cs
+++ mcarvalho/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace mcarvalho.Controllers
             return View();
         }
 
+        public IActionResult Teste()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
